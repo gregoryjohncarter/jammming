f43b51f62e970b5c62c350c88826b79bb4e5f0c6
gregoryjohncarter <gregoryjohncarter@gmail.com>
2022-07-10 15:02:44 -0400

    searchresults comp. added

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearchResults.css';
+import TrackList from '../TrackList/TrackList.js';
+
+const SearchResults = () => {
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      {/* Add a TrackList component */}
+    </div>
+  )
+}
+
+export default SearchResults;
