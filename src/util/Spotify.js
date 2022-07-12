547cdb052d766a7b1edd16fec61b43953c596e97
gregoryjohncarter <gregoryjohncarter@gmail.com>
2022-07-12 16:31:30 -0400

    spotify auth module added

---
+++ src/util/Spotify.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const clientID = process.env.CLIENT_ID;
+const redirectURI = "http://localhost:3000/";
+let accessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    } 
+
+    let tokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    let expiresMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (tokenMatch && expiresMatch) {
+      accessToken = tokenMatch[1];
+      const expiresIn = Number(expiresMatch[1]);
+
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
+      window.location = accessUrl;
+    }
+  }
+};
+
+export default Spotify;
